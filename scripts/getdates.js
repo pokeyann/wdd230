d9e75aec26897da4adecfb43226d978a0e10b350
pokeyann <pokeyann@gmail.com>
2024-01-13 19:56:09 -0600

    footer

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let currentYearElement = document.getElementById("currentYear");
+currentYearElement.textContent = currentYear;
+
+let lastModified = document.lastModified;
+let dateModified = document.getElementById("lastmodified");
+dateModified.innerHTML = lastModified;
+
