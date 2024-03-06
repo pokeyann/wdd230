9bba7836090711e897df28195ed5b455bf4ec588
pokeyann <pokeyann@gmail.com>
2024-03-05 20:32:24 -0600

    fix event listener and hamburger menu

--- chamber/scripts/sidemenu.js
+++ chamber/scripts/sidemenu.js
@@ -2,7 +2,7 @@
 const mainnav = document.querySelector(".navigation")
 const hambutton = document.querySelector("#menu")
 
-hambutton.addEventListner('click', () => {
+hambutton.addEventListener('click', () => {
   mainnav.classList.toggle('show');
   hambutton.classList.toggle('show');
 });
