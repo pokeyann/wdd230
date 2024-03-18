6ad99dd7f3698aedeffc66de9fefdd60f43c843e
pokeyann <pokeyann@gmail.com>
2024-03-18 00:11:03 -0500

    hamburger button fixes

--- scripts/responsivemenu.js
+++ scripts/responsivemenu.js
@@ -5,3 +5,11 @@ hambutton.addEventListener("click", () => {
   navigation.classList.toggle("show");
   hambutton.classList.toggle("show");
 });
+
+/*const mainnav = document.querySelector(".navigation")
+const hambutton = document.querySelector("#menu")
+
+hambutton.addEventListener('click', () => {
+  mainnav.classList.toggle('show');
+  hambutton.classList.toggle('show');
+});*/
