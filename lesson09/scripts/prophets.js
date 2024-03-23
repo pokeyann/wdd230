f493cb3969ff2d893343cbd79e2c14a5199b718a
pokeyann <pokeyann@gmail.com>
2024-03-23 00:59:23 -0500

    prophets js built but can't get dev tools console to work

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,3 +1,4 @@
+/*
 const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
 const cards = document.querySelector("#cards");
 
@@ -8,3 +9,37 @@ async function getProphetData() {
 }
 
 getProphetData();
+*/
+
+const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const cards = document.querySelector("#cards");
+
+async function getProphetData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  //console.table(data.prophets);
+  displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    let card = document.createElement("section");
+    let fullName = document.createElement("h2");
+    let portrait = documnet.createElement("img");
+
+    fullName.textContent = "${prophet.name} lastName";
+
+    portrait.setAttribute("src", prophet.imageurl);
+    portrait.setAttribute("alt", "Potrait of ${prophet.name} lastName");
+    portrait.setAttribute("loading", "lazy");
+    portrait.setAttribute("width", "300");
+    portrait.setAttribute("height", "400");
+
+    card.appendChild(fullName);
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+  });
+}
