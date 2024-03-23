5fb2456febde063a957fd5bea90a690e278d114b
pokeyann <pokeyann@gmail.com>
2024-03-23 13:58:44 -0500

    members json and js

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,1 +1,10 @@
-const 
+const url = "https://github.com/pokeyann/wdd230/";
+const membersurl = "https://github.com/pokeyann/wdd230/blob/main/chamber/data/members.json";
+
+async function getMembersData() {
+  const results = await fetch(membersurl);
+  const data = await results.json();
+  console.log(data);
+}
+
+getMembersData();
