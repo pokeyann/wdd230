a2a48d3e4b60f970503eafadff840604aa98b18c
pokeyann <pokeyann@gmail.com>
2024-03-23 03:22:41 -0500

    I'm clearly doing something wrong with the console

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,15 @@
 const baseURL = "https://github.com/pokeyann/wdd230/";
 
-const linksURL = 
+const linksURL = "https://pokeyann.github.io/wdd230/data/links.json";
+
+console.log(testing);
+async function getLinksData() {
+  console.log("calling");
+  const result = await fetch(linksURl);
+  consult.log(result);
+  const data = await result.json();
+  console.log(data);
+
+}
+
+getLinksData();
