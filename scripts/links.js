1d20aa9500ad33bfdd6e78b442185dbbe541f7b0
pokeyann <pokeyann@gmail.com>
2024-03-25 23:50:18 -0500

    lesson fetch api

--- scripts/links.js
+++ scripts/links.js
@@ -1,6 +1,7 @@
-const baseURL = "https://github.com/pokeyann/wdd230/";
+//const baseURL = "https://github.com/pokeyann/wdd230/";
 
 const linksURL = "https://pokeyann.github.io/wdd230/data/links.json";
+const weeks = document.querySelector(".weeks");
 
 console.log("testing");
 
@@ -9,8 +10,35 @@ async function getLinksData() {
   const result = await fetch(linksURL);
   console.log(result);
   const data = await result.json();
-  console.log(data);
+  console.log(data.lessons);
 
+  displayLinks(data.lessons);
 }
 
 getLinksData();
+
+const displayLinks = (lessons) => {
+  lessons.forEach((week) => {
+    let lesson = document.createElement("section");
+    let url = document.createElement("li");
+    let title = document.createElement("a");
+
+    lesson.appendChild(url);
+    lesson.appendChild(title);
+
+    weeks.appendChild(lesson);
+
+  });
+
+
+}
+/*
+json: lessons lessons url title
+html: 
+div
+section
+h3
+ul 
+li
+a
+href*/
